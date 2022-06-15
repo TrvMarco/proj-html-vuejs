312b18ac9f8bd7f63cc3bc11ca00fadca2d16a6e
TrvMarco <trevisanello.marco@gmail.com>
2022-06-15 17:18:01 +0200

    Aggiunto bottone sezione work w us

--- src/Components/Sections/MainWorkWithUs.vue
+++ src/Components/Sections/MainWorkWithUs.vue
@@ -1,7 +1,10 @@
 <template>
   <section class="work-with-us text-center">
-    <h2 class="text-center display-4">Want to work with us? Send us <span class="italic-font">a message!</span></h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas obcaecati dolorem placeat, quis, exercitationem aliquid accusamus qui, corporis earum harum recusandae dicta. Inventore, corrupti optio! Ea cum fuga accusamus necessitatibus.</p>
+    <div class="container">
+        <h2 class="text-center display-4">Want to work with us? Send us <span class="italic-font">a message!</span></h2>
+        <p class="p-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas obcaecati dolorem placeat, quis, exercitationem aliquid accusamus qui, corporis earum harum recusandae dicta. Inventore, corrupti optio! Ea cum fuga accusamus necessitatibus.</p>
+        <button class="btn-green">Contact</button>
+    </div>
   </section>
 </template>
 
