43d355285fb936ec125818ac9e8e768375996cc4
TrvMarco <trevisanello.marco@gmail.com>
2022-06-16 11:55:54 +0200

    Creato bottoni slider Hero

--- src/Components/Sections/MainHeroSlider.vue
+++ src/Components/Sections/MainHeroSlider.vue
@@ -2,15 +2,21 @@
   <section class="hero-slider">
     <div class="container-fluid">
     <div class="row align-items-center">
-        <div class="col-lg-6 d-flex justify-content-end">
+        <div class="col-lg-6 d-flex justify-content-between align-items-center">
+            <button class="btn-circle">
+                    <i class="fa-solid fa-chevron-left"></i>
+            </button>
             <div class="hero-text">
-                <h1 class="display-2">Devotion that never <span class="italic-font">ends</span></h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed voluptates quia nemo iusto asperiores aliquid omnis.</p>
-                <button>Read More</button>
+                <h1 class="display-2 pb-3">Devotion that never <span class="italic-font">ends</span></h1>
+                <p class="pb-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed voluptates quia nemo iusto asperiores aliquid omnis.</p>
+                <button class="btn-custom btn-border-pink">Read More</button>
             </div>
         </div>
-        <div class="col-lg-6">
-            <img src="../../assets/img/hero-slider.png" alt="Art image slider">
+        <div class="col-lg-6 d-flex justify-content-between align-items-center">
+            <img class="img-fluid" src="../../assets/img/hero-slider.png" alt="Art image slider">
+            <button class="btn-circle">
+                <i class="fa-solid fa-chevron-right"></i>
+            </button>
         </div>
     </div>
   </div>
--- src/Components/Sections/MainWorkWithUs.vue
+++ src/Components/Sections/MainWorkWithUs.vue
@@ -3,7 +3,7 @@
     <div class="container">
         <h2 class="text-center display-4">Want to work with us? Send us <span class="italic-font">a message!</span></h2>
         <p class="p-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas obcaecati dolorem placeat, quis, exercitationem aliquid accusamus qui, corporis earum harum recusandae dicta. Inventore, corrupti optio! Ea cum fuga accusamus necessitatibus.</p>
-        <button class="btn-green">Contact</button>
+        <button class="btn-custom btn-green">Contact</button>
     </div>
   </section>
 </template>
@@ -16,7 +16,7 @@ export default {
 
 <style lang="scss" scoped>
     section{
-        padding: var(--padding-section);
-        background: var(--main-bg-color);
+      padding: var(--padding-section);
+      background: var(--main-bg-color);
     }
 </style>
